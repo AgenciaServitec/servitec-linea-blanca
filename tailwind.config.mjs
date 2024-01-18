e44ab4d7991b057a74981a57ccd65ac93ede75cd
Angel Gala <galafloresangelemilio@gmail.com>
2024-01-18 17:35:51 -0500

    advance of project

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {
       colors: {
@@ -45,14 +48,27 @@ export default {
         },
       },
       width: {
-        "98": "26rem",
-        "100": "30rem"
+        98: "26rem",
+        100: "30rem",
       },
       height: {
-        "98": "26rem",
-        "100": "30rem"
-      }
+        98: "26rem",
+        100: "30rem",
+      },
+      fontFamily: {
+        sans: ["Nunito Variable", "sans-serif"],
+      },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin"), require("daisyui")],
+  daisyui: {
+    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "light", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: false, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 };
