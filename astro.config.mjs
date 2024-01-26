00472235f37478454e837e51caa56bf8b1390d6e
Angel Gala <galafloresangelemilio@gmail.com>
2024-01-26 17:31:36 -0500

    add waves in the home

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://airservitec.com",
-  integrations: [react(), tailwind()]
+  integrations: [icon(), react(), tailwind()]
 });
