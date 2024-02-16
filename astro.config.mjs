b5654675c8ce219d1a6c4459f304379d2eefac86
Angel Gala <galafloresangelemilio@gmail.com>
2024-02-16 12:59:46 -0500

    add button call

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-import icon from "astro-icon";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://servitec-reparacion-electrodomesticos.com",
-  integrations: [icon(), react(), tailwind()]
+  integrations: [react(), tailwind()]
 });
